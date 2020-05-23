3c167c1e112f6b5eaad0a8adf1cdd26da6c3b955
kawehburg <46196245+kawehburg@users.noreply.github.com>
2020-05-23 19:31:46 +0800

    initial

---
+++ problems/5.py
@@ -0,0 +1,26 @@
+from utils.utils import container, random_str
+from collections import defaultdict
+import random
+
+
+def func(s):
+    cache = defaultdict(list)
+    center = defaultdict(int)
+    for i, item in enumerate(s):
+        for last in cache[item]:
+            c = (last + i) / 2
+            hl = i - last + 1
+            if hl - center[c] <= 3:
+                center[c] = hl
+        cache[item].append(i)
+    max_c = 0
+    keys = list(center.keys())
+    for c in keys:
+        if center[c] > center[max_c]:
+            max_c = c
+    if center[max_c] == 0:
+        center[max_c] = 1
+    return s[int(max_c - center[max_c] / 2 + 0.5): int(max_c + center[max_c] / 2 + 0.5)]
+
+
+container(func, 'abc')
---
+++ utils/utils.py
@@ -0,0 +1,20 @@
+import time
+# from memory_profiler import profile
+import sys
+import random
+
+
+def container(run, *args, **kwargs):
+    t = time.time()
+    results = run(*args, **kwargs)
+    print('time cost=', time.time() - t)
+    print('results =', results)
+
+
+def random_str(slen=10):
+    # seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
+    seed = 'qwertyuiopasdfghjklzxcvbnm'
+    sa = []
+    for i in range(slen):
+        sa.append(random.choice(seed))
+    return ''.join(sa)
