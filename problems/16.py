83dcace3158fa6a67427ea1168672985896d430e
kawehburg <46196245+kawehburg@users.noreply.github.com>
2020-05-24 22:43:05 +0800

    add red

--- problems/16.py
+++ problems/16.py
@@ -29,6 +29,7 @@ def func(nums, target):
                     else:
                         return True
         return False
+
     if epoch(target):
         return target
     m = 0
@@ -38,6 +39,17 @@ def func(nums, target):
         if epoch(target - m):
             return target - m
         m += 1
+
+
+def ref(nums, target):
+    import bisect
+    nums, r, end = sorted(nums), float('inf'), len(nums) - 1
+    for c in range(len(nums) - 2):
+        i, j = max(c + 1, bisect.bisect_left(nums, target - nums[end] - nums[c], c + 1, end) - 1), end
+        while r != target and i < j:
+            s = nums[c] + nums[i] + nums[j]
+            r, i, j = min(r, s, key=lambda x: abs(x - target)), i + (s < target), j - (s > target)
+    return r
 
 
 container(func, [-1, 2, 1, -4], 1)
